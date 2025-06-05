519c6f7c306c2d68846bb3ebfeeeb69c429e2122
Norberthue <kokanorbert9@gmail.com>
2025-06-05 18:30:59 +0200

    ERD

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import { data } from './transactions.js'
 
+//TODO: 1. Vyfiltrovat transkacie podla to ci obsahuju amount, createdAt (timestamp) , customerName a state sa rovna canceled
+// 2. Sortovat transkacie podla timeStampu od najnovych po nastarsie
+// 3. Zoskupit transkaie podla roku
+
 const canceledTransactions = (transactions) => {
   return transactions
-    .filter(data =>
-      data.state === "canceled" &&
-      data.amount &&
+    .filter(data => 
+      data.state === 'canceled' &&
       data.createdAt &&
-      data.customerName 
+      data.customerName && 
+      data.amount
     )
     .sort((a, b) => b.createdAt - a.createdAt)
     .reduce((acc, item) => {
@@ -15,12 +19,11 @@ const canceledTransactions = (transactions) => {
       if (!acc[year]) {
         acc[year] = []
       }
-      
-      acc[year].push(item)
-      
-      return acc
-    }, {})
-}
 
+      acc[year].push(item)
+
+      return acc
+  }, {})
+}
 
 console.log(canceledTransactions(data))
